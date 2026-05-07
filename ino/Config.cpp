de483192fcc4130a46fb18cadff0088178fd1223
pBielli <patrizio.bielli1998@gmail.com>
2026-05-07 02:09:37 +0200

    fast commit

--- ino/Config.cpp
+++ ino/Config.cpp
@@ -20,28 +20,16 @@ void storageSave() {
 }
 
 void storageReset() {
-  strncpy(cfg.wifi_ssid, "CasaBielli", 31);
-  strncpy(cfg.wifi_password, "CasaBielli01", 63);
-  strncpy(cfg.mdns_name, "ddns-checker", 31);
-  strncpy(cfg.admin_password, "admin", 31);
-  strncpy(cfg.ddns_hostname, "patb.duckdns.org", 63);
-  strncpy(cfg.duckdns_token, "3894523a-d53d-433d-b661-83ac156319fa", 47);
-  strncpy(cfg.duckdns_domain, "patb", 31);
+  strncpy(cfg.wifi_ssid, "YOUR_SSID", 31);
+  strncpy(cfg.wifi_password, "YOUR_PASSWORD", 63);
+  strncpy(cfg.mdns_name, "YOUR_MDNS_NAME", 31);
+  strncpy(cfg.admin_password, "YOUR_ADMIN_PASSWORD", 31);
+  strncpy(cfg.ddns_hostname, "YOUR_DUCKDNS_HOSTNAME", 63);
+  strncpy(cfg.duckdns_token, "YOUR_DUCKDNS_TOKEN", 47);
+  strncpy(cfg.duckdns_domain, "YOUR_DUCKDNS_DOMAIN", 31);
   cfg.led_pin = 2;
   storageSave();
 }
-
-// void storageReset() {
-//   strncpy(cfg.wifi_ssid, "YOUR_SSID", 31);
-//   strncpy(cfg.wifi_password, "YOUR_PASSWORD", 63);
-//   strncpy(cfg.mdns_name, "YOUR_MDNS_NAME", 31);
-//   strncpy(cfg.admin_password, "YOUR_ADMIN_PASSWORD", 31);
-//   strncpy(cfg.ddns_hostname, "YOUR_DUCKDNS_HOSTNAME", 63);
-//   strncpy(cfg.duckdns_token, "YOUR_DUCKDNS_TOKEN", 47);
-//   strncpy(cfg.duckdns_domain, "YOUR_DUCKDNS_DOMAIN", 31);
-//   cfg.led_pin = 2;
-//   storageSave();
-// }
 
 
 bool storageInitialized() {
